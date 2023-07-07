608f7c74e85625f024c6bef544be69b3b2bdb50b
Prabhdeep Singh <prsi744@ess.ais.ac.nz>
2023-07-07 13:55:20 +1200

    first commit

---
+++ config/database.js
@@ -0,0 +1,36 @@
+// database module
+var mysql = require('mysql2');
+var config = {
+    host: 'localhost',
+    user: 'root',
+    password: 'PD87@mysql',
+    database: 'aStore'
+};
+
+// init database
+var pool = mysql.createPool(config);
+
+//Fetch data
+function RunQuery(sql, callback) {
+    pool.getConnection(function (err, conn) {
+        if (err) {
+            ShowErrors(err);
+        }
+        conn.query(sql, function (err, rows, fields) {
+            if (err) {
+                ShowErrors(err);
+            }
+            conn.release();
+            callback(rows);
+        });
+    });
+}
+
+//Throw errors
+function ShowErrors(err) {
+    throw err;
+}
+
+module.exports = {
+    RunQuery: RunQuery
+};
